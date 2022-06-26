d86bfac59d0a4822d78ce1f297eb82d451d7fcdd
klaudiawuu <klaudia.wawrzynczyk@gmail.com>
2022-06-26 13:30:05 +0200

    axios added to the app

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+
+    let apiKey = "a367566821d5256a1c920a360eab8e9e";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+    function showWeather(response) {
+        alert(`The weather in ${response.data.name} is ${response.data.main.temp} degrees`);
+    }
+
+    axios.get(apiUrl).then(showWeather);
+}
